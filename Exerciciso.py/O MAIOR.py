dbbfd33ad0b46dca9fa39fdb1c1ee8f41c305bc1
motadp <smotadp@gmail.com>
2021-12-25 19:13:59 -0300

    Maior

--- Exerciciso.py/O MAIOR.py
+++ Exerciciso.py/O MAIOR.py
@@ -1,10 +1,12 @@
-
 a,b,c = input().split(" ")
+
+a = int(a)
+b = int(b)
+c = int(c)
 
 num_maior = (a+b+abs(a-b))/2
 
-num_maior = (num_maior + c + abs(num_maior - c))/2
+num_maior = int((num_maior + c + abs(num_maior - c))/2)
+
 
 print(f"{num_maior} eh o maior")
-
-
